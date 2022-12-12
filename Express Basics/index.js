9452e06041e73cd6e527bc710ef156f16880162f
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-12-12 14:47:42 +0545

    app.js renamed  to index.js

--- Express Basics/index.js
+++ Express Basics/index.js
@@ -5,6 +5,12 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get("/ramesh", (req, res)=>{
+  res.send("Hello Ramesh")
+})
+app.get("/features", (req, res)=>{
+  res.send("This is a feature section")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
